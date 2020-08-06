77365a9e74876a5b07a0bce097c3a0be29a40957
Andrea Nurce <andrea-dibra@hotmail.com>
2020-08-06 15:51:27 +0200

    Adding files to the repository

---
+++ style.js
@@ -0,0 +1,151 @@
+//this function ope the tab that styles the page background
+
+function toggle() {
+    document.getElementById('toogle').style.display='none';
+    document.getElementById('secondImage').style.display='block';
+    document.getElementById('itemList').style.display = "block";
+}
+//this function  close the tab that styles the page background
+function origin() {
+  document.getElementById('toogle').style.display='block';
+  document.getElementById('secondImage').style.display='none';
+  document.getElementById('itemList').style.display = "none";
+}
+//this paragraph ridirect you somewhere else when you succesfully log in
+function redirect() {
+  let pass1 = document.getElementById('password').value;
+  let pass2 = document.getElementById('confirmPassword').value;
+  if (pass1 === pass2) {
+    window.location.replace("");
+  }else{
+    alert('Sorry something went wrong!!!')
+  }
+}
+
+//here starts the section that styles the background of the page
+
+ function bgColor1() {
+   var backgroundColore = document.getElementById("bg1").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor2() {
+   var backgroundColore = document.getElementById("bg2").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor3() {
+   var backgroundColore = document.getElementById("bg3").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor4() {
+   var backgroundColore = document.getElementById("bg4").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor5() {
+   var backgroundColore = document.getElementById("bg5").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor6() {
+   var backgroundColore = document.getElementById("bg6").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor7() {
+   var backgroundColore = document.getElementById("bg7").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor8() {
+   var backgroundColore = document.getElementById("bg8").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor9() {
+   var backgroundColore = document.getElementById("bg9").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function bgColor10() {
+   var backgroundColore = document.getElementById("bg10").style.backgroundColor;
+   var x = document.getElementsByTagName("BODY")[0];
+   x.style.backgroundColor = backgroundColore;
+
+ }
+
+//here starts the section to tyle the form background
+ function fmColor1() {
+   var backgroundColore = document.getElementById("fm1").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+
+ function fmColor2() {
+   var backgroundColore = document.getElementById("fm2").style.backgroundColor;
+   var x = document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor3() {
+   var backgroundColore = document.getElementById("fm3").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor4() {
+   var backgroundColore = document.getElementById("fm4").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor5() {
+   var backgroundColore = document.getElementById("fm5").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+
+
+ function fmColor6() {
+   var backgroundColore = document.getElementById("fm6").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor7() {
+   var backgroundColore = document.getElementById("fm7").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor8() {
+   var backgroundColore = document.getElementById("fm8").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor9() {
+   var backgroundColore = document.getElementById("fm9").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
+ function fmColor10() {
+   var backgroundColore = document.getElementById("fm10").style.backgroundColor;
+   var x =document.getElementById('loogin');
+   x.style.backgroundColor = backgroundColore;
+
+ }
